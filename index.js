1d12039a6e1a0bb8009a6ca23238c2b4d0b7c189
joeldevel <christianjoel.developer@gmail.com>
2021-09-09 09:44:32 -0300

    change url to get current weather. fix tipos

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
-const BASE_URL = "http://api.openweathermap.org";
-let query = "data/2.5/forecast?";
+const BASE_URL = "http://api.openweathermap.org/data/2.5";
+let query = "weather?";
+let cityName = "London";
 let id = "524901";
-let url  = BASE_URL + "/" + query + "id=" + id + "&appid=" + API_KEY;
-
+let url  = BASE_URL + "/" + query + "q=" + cityName + "&appid=" + API_KEY;
+console.log(url);
 fetch(url, {mode: 'cors'})
   .then((response) => response.json())
   .then(data=>console.log(data));
