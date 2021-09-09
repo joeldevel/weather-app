1d1a5a43ba00200206b8543fd83622583234a45c
joeldevel <christianjoel.developer@gmail.com>
2021-09-09 10:06:09 -0300

    add function to get data given a location

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 const BASE_URL = "http://api.openweathermap.org/data/2.5";
 let query = "weather?";
-let cityName = "London";
-let id = "524901";
-let url  = BASE_URL + "/" + query + "q=" + cityName + "&appid=" + API_KEY;
-console.log(url);
-fetch(url, {mode: 'cors'})
+// let id = "524901";
+// console.log(url);
+
+
+
+function getWeatherData(location) {
+  let cityName = location;
+  let url  = BASE_URL + "/" + query + "q=" + cityName + "&appid=" + API_KEY;
+  fetch(url, {mode: 'cors'})
   .then((response) => response.json())
-  .then(data=>console.log(data));
+  .then(data => console.log(data));
+}
+
+getWeatherData("Buenos Aires");
