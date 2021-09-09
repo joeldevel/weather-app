bf3bdc3dd3b6164c13db38fc63a912a2d593ab54
joeldevel <christianjoel.developer@gmail.com>
2021-09-09 09:33:36 -0300

    can fetch data

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("API_KEY = ", API_KEY);
+
+const BASE_URL = "http://api.openweathermap.org";
+let query = "data/2.5/forecast?";
+let id = "524901";
+let url  = BASE_URL + "/" + query + "id=" + id + "&appid=" + API_KEY;
+
+fetch(url, {mode: 'cors'})
+  .then((response) => response.json())
+  .then(data=>console.log(data));
