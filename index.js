15f833730980e6ab6de224fe5fbe0ccdf9a9c7e1
joeldevel <christianjoel.developer@gmail.com>
2022-01-20 01:08:07 -0300

    adds basic UI, can retrieve data from static city and show name, temp and icon

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function getWeatherData(location) {
 function processJSONData(data) {
   // console.log(data);
   let parcialData = {
+    name: data['name'],
     weather: data['weather'],
     main: data['main'],
     wind: data['wind'],
@@ -35,11 +36,41 @@ function processJSONData(data) {
   return parcialData;
 }
 
-getWeatherData("Buenos Aires")
-    .then(result => console.log(result));
-    // .then(result=>console.log(kelvinToCelsius(Number(result.temp))));
-// console.log(getWeatherData("Buenos Aires"));
+// getWeatherData("Buenos Aires")
+//     .then(result => console.log(result));
+
+
+function iconURL(id) {
+    return `http://openweathermap.org/img/wn/${id}@2x.png`;
+}
 
 function kelvinToCelsius(kelvin) {
     return (kelvin - 273.15);
 }
+
+/*****************************************************************************
+//                        get user input
+******************************************************************************/
+
+const city = document.querySelector('#user-input');
+const form = document.querySelector('#form');
+const cardTitle = document.querySelector('#city-name');
+const cityTemperature = document.querySelector('#city-temperature');
+const weatherIcon = document.querySelector('#weather-icon');
+// console.log(weatherIcon);
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    // console.log("sending info");
+    if(city.value !== "") {
+        // console.log("yea, somthing to read");
+        getWeatherData("Buenos Aires")
+            .then(result => {
+              // console.log(result);
+              cardTitle.textContent = result.name;
+              cityTemperature.textContent = result.main.temp;
+              weatherIcon.src = iconURL(result.weather[0].icon);
+        });
+    }
+})
+// console.log(city)
