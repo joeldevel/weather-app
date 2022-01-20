ca464661cc258cc67661c249294b566fb0cab7e1
joeldevel <christianjoel.developer@gmail.com>
2022-01-19 23:59:19 -0300

    adds functionality to processJSON function

--- index.js
+++ index.js
@@ -9,21 +9,37 @@ let query = "weather?";
 function getWeatherData(location) {
   let cityName = location;
   let url  = BASE_URL + "/" + query + "q=" + cityName + "&appid=" + API_KEY;
-  fetch(url, {mode: 'cors'})
-  .then((response) => response.json())
-  .then(data => {
-    // console.log(data)
-    currentWeather = processJSONData(data);
-    console.log(currentWeather);
-  });
+  // let temperature = fetch(url, {mode: 'cors'})
+  //                     .then((response) => response.json())
+  //                     .then(data => {
+  //                         currentWeather = processJSONData(data);
+  //                         return currentWeather;
+  //                     });
+  // return temperature.then(x=>x);
+
+  return fetch(url, {mode: 'cors'})
+              .then((response) => response.json())
+              .then(data => {
+                  currentWeather = processJSONData(data);
+                  return currentWeather;
+              });
 }
 
 function processJSONData(data) {
-  weather = {
-    "temp": data.main.temp
-  };
-
-  return weather;
+  // console.log(data);
+  let parcialData = {
+    weather: data['weather'],
+    main: data['main'],
+    wind: data['wind'],
+  }
+  return parcialData;
 }
 
-getWeatherData("Buenos Aires");
+getWeatherData("Buenos Aires")
+    .then(result => console.log(result));
+    // .then(result=>console.log(kelvinToCelsius(Number(result.temp))));
+// console.log(getWeatherData("Buenos Aires"));
+
+function kelvinToCelsius(kelvin) {
+    return (kelvin - 273.15);
+}
